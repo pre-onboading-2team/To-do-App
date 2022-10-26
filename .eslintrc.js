ab2a14af7b701ee099ac7c2158b2436591f00fbf
jinseok_kim <genuineseok@gmail.com>
2022-10-26 14:54:19 +0900

    Chore: ESLint, Prettier 설정 파일 추가, husky 설정 추가
    - eslint config, plugin 개발 모드 패키지 추가
    - postinstall, lint, format 스크립트 추가
    - husky pre-commit, pre-push 스크립트 추가

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+	// 런타임 환경 정의 : node, browser 모두
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	// react 기본 config + prettier
+	extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	// react 기본 plugin
+	plugins: ['react'],
+	rules: {
+		"react/jsx-uses-react": "off",
+		"react/react-in-jsx-scope": "off"
+	},
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	singleQuote: true, // 작은 따옴표 사용
+	semi: true, // 세미콜론 자동 주입
+	useTabs: true, // 들여쓰기 tab으로 대체
+	tabWidth: 2, // 들여쓰기 단위
+	trailingComma: 'all', // 여러 줄을 사용할 때, 후행 콤마 사용 여부
+	printWidth: 80, // 줄 바꿈 폭 넓이
+};
