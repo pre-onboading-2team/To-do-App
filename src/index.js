83460b4343748f8e76f95f3eebb9b4875a68bcbf
CSH111 <seongho6022@naver.com>
2022-10-27 18:24:26 +0900

    Rename: GlobalStyles 경로 수정 및 styles폴더 삭제
    - /src/styles 삭제
    - /src/styles/GlobalStyles.js => /src/components/Globalstyles.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import GlobalStyles from "./styles/GlobalStyles";
+import GlobalStyles from "./components/GlobalStyles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
