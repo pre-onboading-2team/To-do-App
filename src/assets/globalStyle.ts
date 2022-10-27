d595511ac9336f4e3ddbf68800e33821a698d564
MoSangBin <mosangbin94@gmail.com>
2022-10-27 15:59:57 +0900

    Stlye: globalStlye 파일 분리

---
+++ src/assets/globalStyle.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  
+  * {
+    box-sizing: border-box;
+  }
+ 
+  html,
+  body {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
+  ol, ul, li {
+    list-style: none;
+  }
+  a {
+    text-decoration: none;
+    &:link,
+    &:visited {
+      color: inherit;
+    }
+  }
+  button {
+    cursor: pointer;
+  }
+`;
